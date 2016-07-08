105fc1d6b4638b291a583a4b53b5f24517db97a8
EPiQC <ajavadia@uchicago.edu>
2016-07-07 18:04:51 -0700

    Added Regression Tests

---
+++ scripts/get_total_gates_line.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import sys
+
+the_file = open(sys.argv[1],"r")
+for line in the_file.readlines():
+    if line.startswith( 'total_gates' ):
+        print line
+the_file.close()
---
+++ scripts/make_regression_testcase.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+ROOT=$DIR/..
+OPT=$ROOT/build/Release+Asserts/bin/opt
+SCAF=$ROOT/build/Release+Asserts/lib/Scaffold.so
+
+rm -rf $ROOT/test_cases
+mkdir $ROOT/test_cases
+for testcase in Cat_State Square_Root; do
+#for testcase in Cat_State QFT Square_Root; do
+    mkdir $ROOT/test_cases/$testcase
+    cp -R $ROOT/Algorithms/$testcase  $ROOT/test_cases
+    $ROOT/scaffold.sh -R $ROOT/test_cases/$testcase/*.scaffold > $ROOT/test_cases/$testcase/tmp.txt
+    python get_total_gates_line.py $ROOT/test_cases/$testcase/tmp.txt  > $ROOT/test_cases/$testcase/total_gates.txt
+    $ROOT/scaffold.sh -q -R $ROOT/test_cases/$testcase/*.scaffold 
+    cp *.qasmh  $ROOT/test_cases/$testcase
+    $ROOT/scaffold.sh -f -R $ROOT/test_cases/$testcase/*.scaffold
+    cp *.qasmf  $ROOT/test_cases/$testcase
+    rm -rf $ROOT/test_cases/$testcase/tmp.txt
+    $ROOT/scaffold.sh -c $ROOT/test_cases/$testcase/*.scaffold
+done
