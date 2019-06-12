e9a2e57027414dfa6b29c713a6acfec03dcdb50a
Yipeng Huang <yipeng@cs.princeton.edu>
2019-06-12 16:09:40 -0400

    Adding more comments on how this script splits the Scaffassert program into several Scaffold programs

--- scripts/simulation/scaffassert.py
+++ scripts/simulation/scaffassert.py
@@ -3,13 +3,17 @@
 import sys, re, copy, os
 
 break_indx = 0
-breakpoints = [[]]
+breakpoints = [[]] # the Scaffold code to be generated for each breakpoint
 
+# open the Scaffassert file as input
 with open(sys.argv[1]) as source:
+    # for all the lines of code in the Scaffassert program...
     for line in source:
 
+        # decide if the line is any of the three types of assertions at all
         if re.findall("^assert_", line.strip()):
 
+	        # syntax: assert_classical ( quantum_register, register_width, assertion_value )
             if "assert_classical" in line:
                 # start the next breakpoint
                 breakpoints.append(copy.copy(breakpoints[break_indx]))
@@ -17,12 +21,12 @@ with open(sys.argv[1]) as source:
                 param_string = re.findall("^assert_classical\s*\((.*?)\)\s*;", line.strip())
                 params = [param.strip() for param in param_string[0].split(',')]
 
-                # close out this breakpoint
+                # close out this breakpoint by doing measurement
                 breakpoints[break_indx].append('for ( int i=0 ; i<{:s} ; i++ )\n'.format(params[1]))
                 breakpoints[break_indx].append('    MeasZ ( {:s}[i] );\n'.format(params[0]))
                 breakpoints[break_indx].append('}\n')
 
-                # generate script for checking the breakpoint
+                # generate script for checking this breakpoint after simulation and measurement
                 breakpoint_name = os.path.splitext(os.path.basename(sys.argv[1]))[0] + '.breakpoint_' + str(break_indx+1)
                 with open(breakpoint_name + '.bash', 'w') as outfile:
                     outfile.write('$SCAFFCC_PATH/scripts/simulation/assert_classical.py {:s}.csv {:s} {:s} {:s}'.format(
@@ -33,6 +37,7 @@ with open(sys.argv[1]) as source:
 
                 break_indx+=1
 
+	        # syntax: assert_superposition ( quantum_register, register_width )
             elif "assert_superposition" in line:
                 # start the next breakpoint
                 breakpoints.append(copy.copy(breakpoints[break_indx]))
@@ -40,12 +45,12 @@ with open(sys.argv[1]) as source:
                 param_string = re.findall("^assert_superposition\s*\((.*?)\)\s*;", line.strip())
                 params = [param.strip() for param in param_string[0].split(',')]
 
-                # close out this breakpoint
+                # close out this breakpoint by doing measurement
                 breakpoints[break_indx].append('for ( int i=0 ; i<{:s} ; i++ )\n'.format(params[1]))
                 breakpoints[break_indx].append('    MeasZ ( {:s}[i] );\n'.format(params[0]))
                 breakpoints[break_indx].append('}\n')
 
-                # generate script for checking the breakpoint
+                # generate script for checking this breakpoint after simulation and measurement
                 breakpoint_name = os.path.splitext(os.path.basename(sys.argv[1]))[0] + '.breakpoint_' + str(break_indx+1)
                 with open(breakpoint_name + '.bash', 'w') as outfile:
                     outfile.write('$SCAFFCC_PATH/scripts/simulation/assert_superposition.py {:s}.csv {:s} {:s}'.format(
@@ -91,4 +96,5 @@ for break_indx in range(len(breakpoints)):
         for line in breakpoints[break_indx]:
             outfile.write(line)
 
+# tell all the subsequent scripts how many breakpoints we found here
 print (break_indx+1)
